32ed163b8d944246ae33bff93cf105dec43f1ec4
Gustavo V. Castro <gustavovalcastro@gmail.com>
2021-08-05 23:32:47 -0300

    Add: Armazenamento de entradas em exe03/main.c

--- exe02/main.c
+++ exe02/main.c
@@ -2,9 +2,11 @@
 
 int main() {
 	//Entrada
-	int n1, n2;
-	scanf("%i %i", &n1, &n2);
+	int consumo, tempo, rendimento, velocidade;
+	printf("Insira entre espaços o tempo percorrido, velocidade e rendimento médio do veículo: ");
+	scanf("%i %i %i", &tempo, &rendimento, &velocidade);
 	//PROCESSAMENTO
+	consumo = (tempo/velocidade)/rendimento
 	printf("SOMA = %i\n", n1+n2);
 	return 0;
 }
