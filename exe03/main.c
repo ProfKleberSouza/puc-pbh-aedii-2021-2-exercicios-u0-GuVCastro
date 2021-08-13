ea448240f63d3f090f0c586a99c5509c24606976
Gustavo V. Castro <gustavovldcastro@gmail.com>
2021-08-12 22:58:12 -0300

    Lógica do programa exe03/main.c criada

--- exe03/main.c
+++ exe03/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+void checa_numero(int numero)
+{
+	if (numero >= 0)
+		if (numero%2 == 0) printf("NÚMERO PAR POSITIVO\n");
+		else printf("NÚMERO ÍMPAR POSITIVO\n");
+	else
+		if (numero%2 == 0) printf("NÚMERO PAR NEGATIVO\n");
+		else printf("NÚMERO ÍMPAR NEGATIVO\n");
+}	
+
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   int numero;
+
+   printf("Informe um número: ");
+   scanf("%d", &numero);
+
+   checa_numero(numero);
+
    return 0;
 }
