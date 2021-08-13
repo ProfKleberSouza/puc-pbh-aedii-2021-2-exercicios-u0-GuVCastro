d81f7494583c56979191a7ab87edb1befeee268d
Gustavo V. Castro <gustavovldcastro@gmail.com>
2021-08-12 23:35:32 -0300

    Conclusão da lógica do programa

--- exe02/main.c
+++ exe02/main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 
+void preenche_dados(int *rendimento, int *velocidade, int *tempo)
+{
+	printf("Insira respectivamente o rendimento, velocidade média e tempo de viagem: ");
+	scanf("%d %d %d", rendimento, velocidade, tempo);
+}
+
+void calcula_valores(int rendimento, int velocidade, int tempo, int *distancia, int *consumo)
+{
+	*distancia = velocidade * tempo;
+	*consumo = *distancia / rendimento;
+}
+
+void imprime_dados(int rendimento, int velocidade, int tempo, int distancia, int consumo)
+{
+	printf("R = %d\n", rendimento);
+	printf("V = %d\n", velocidade);
+	printf("T = %d\n", tempo);
+	printf("D = %d\n", distancia);
+	printf("L = %d\n", consumo);
+}
+
 int main() {
-	int consumo, tempo, rendimento, velocidade;
-	printf("Insira entre espaços o tempo percorrido, velocidade e rendimento médio do veículo: ");
-	scanf("%i %i %i", &tempo, &rendimento, &velocidade);
-	consumo = (tempo/velocidade)/rendimento
-	printf("SOMA = %i\n", n1+n2);
+	int rendimento, velocidade, tempo, distancia, consumo;
+	
+	preenche_dados(&rendimento, &velocidade, &tempo);
+	calcula_valores(rendimento, velocidade, tempo, &distancia, &consumo);
+	imprime_dados(rendimento, velocidade, tempo, distancia, consumo);
+
 	return 0;
 }
