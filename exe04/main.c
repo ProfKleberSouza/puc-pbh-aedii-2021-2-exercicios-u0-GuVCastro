cbcc4aa18f83ba69f844a0626d8c7f55e65c68dc
Gustavo V. Castro <gustavovldcastro@gmail.com>
2021-08-13 00:06:10 -0300

    Lógica do programa exe04 concluída

--- exe04/main.c
+++ exe04/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
+void imprime_extremos(int *numeros)
+{
+	if (numeros[0] > numeros[1] && numeros[0] > numeros[2]) printf("MAIOR = %d\n", numeros[0]);	
+	else if (numeros[1] > numeros[2]) printf("MAIOR = %d\n", numeros[1]);	
+	else printf("MAIOR = %d\n", numeros[2]);
+
+	if (numeros[0] < numeros[1] && numeros[0] < numeros[2]) printf("MENOR = %d\n", numeros[0]);	
+	else if (numeros[1] < numeros[2]) printf("MENOR = %d\n", numeros[1]);	
+	else printf("MENOR = %d\n", numeros[2]);
+}
+
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+	int numeros[3];
+
+	printf("Insira 3 número respctivamente: ");
+	scanf("%d %d %d", &numeros[0], &numeros[1], &numeros[2]);
+
+	imprime_extremos(numeros);
 }
