1ed3c8b29d98e9cb3b8f234808617d75399573b7
Gustavo V. Castro <gustavovldcastro@gmail.com>
2021-08-12 22:41:32 -0300

    Removed comments on exe02/main.c

--- exe02/main.c
+++ exe02/main.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 int main() {
-	//Entrada
 	int consumo, tempo, rendimento, velocidade;
 	printf("Insira entre espaços o tempo percorrido, velocidade e rendimento médio do veículo: ");
 	scanf("%i %i %i", &tempo, &rendimento, &velocidade);
-	//PROCESSAMENTO
 	consumo = (tempo/velocidade)/rendimento
 	printf("SOMA = %i\n", n1+n2);
 	return 0;
