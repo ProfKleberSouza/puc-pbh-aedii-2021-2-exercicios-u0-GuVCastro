c7dee0160b9a1d52cc9780044a0966541374a0bf
Gustavo V. Castro <gustavovldcastro@gmail.com>
2021-08-14 12:18:04 -0300

    Adequação do programa para algoritmo sequencial

--- exe02/main.c
+++ exe02/main.c
@@ -1,32 +1,19 @@
 #include <stdio.h>
 
-void preenche_dados(int *rendimento, int *velocidade, int *tempo)
-{
+int main() {
+	int rendimento, velocidade, tempo, distancia, consumo;
+	
 	printf("Insira respectivamente o rendimento, velocidade média e tempo de viagem: ");
-	scanf("%d %d %d", rendimento, velocidade, tempo);
-}
-
-void calcula_valores(int rendimento, int velocidade, int tempo, int *distancia, int *consumo)
-{
-	*distancia = velocidade * tempo;
-	*consumo = *distancia / rendimento;
-}
-
-void imprime_dados(int rendimento, int velocidade, int tempo, int distancia, int consumo)
-{
+	scanf("%d %d %d", &rendimento, &velocidade, &tempo);
+	
+	distancia = velocidade * tempo;
+	consumo = distancia / rendimento;
+	
 	printf("R = %d\n", rendimento);
 	printf("V = %d\n", velocidade);
 	printf("T = %d\n", tempo);
 	printf("D = %d\n", distancia);
 	printf("L = %d\n", consumo);
-}
-
-int main() {
-	int rendimento, velocidade, tempo, distancia, consumo;
-	
-	preenche_dados(&rendimento, &velocidade, &tempo);
-	calcula_valores(rendimento, velocidade, tempo, &distancia, &consumo);
-	imprime_dados(rendimento, velocidade, tempo, distancia, consumo);
 
 	return 0;
 }
--- exe05/main.c
+++ exe05/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+void recebe_notas(int *notas)
+{
+	
+}
+
+int main()
+{
+	int notas[4];
+
+	recebe_notas(notas)
+
 }
